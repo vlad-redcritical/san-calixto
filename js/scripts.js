5572ffdf1f19b917ea51fa26c89e8b3a062f4a5c
vlad-redcritical <vlad.chikileyski@redcritical.pl>
2018-09-10 01:20:01 +0200

    Infotip implementation & checkbox styles
    Additional tippy js style and minify version. Checkbox fix. Small styles changes.

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,24 @@
  * File: scripts.js
  * Created: 2018-08-09-17:02
  */
+$(document).ready(function () {
 
+ 		 tippy('.infotip-optional', {
+            theme: 'light',
+            arrow: true,
+  			arrowType: 'round',
+            size: 'small',
+            animation: 'fade',
+            delay: [120, 120]
+        });
+
+         tippy('.info-req-value', {
+            theme: 'light',
+            arrow: true,
+  			arrowType: 'round',
+            size: 'small',
+            animation: 'fade',
+            delay: [120, 120]
+        });
+         
+});
